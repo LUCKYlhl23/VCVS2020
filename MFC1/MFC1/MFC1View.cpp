be47bb73c8aa486507725052dd54c5a4f246ad60
LUCKYlhl123 <1291968750@qq.com>
2020-03-24 13:59:23 +0800

    2020—3—23提交之前的实验

---
+++ MFC1/MFC1/MFC1View.cpp
@@ -0,0 +1,91 @@
+﻿
+// MFC1View.cpp: CMFC1View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFC1.h"
+#endif
+
+#include "MFC1Doc.h"
+#include "MFC1View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFC1View
+
+IMPLEMENT_DYNCREATE(CMFC1View, CView)
+
+BEGIN_MESSAGE_MAP(CMFC1View, CView)
+END_MESSAGE_MAP()
+
+// CMFC1View 构造/析构
+
+CMFC1View::CMFC1View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMFC1View::~CMFC1View()
+{
+}
+
+BOOL CMFC1View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFC1View 绘图
+
+void CMFC1View::OnDraw(CDC* pDC)
+{
+	CMFC1Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+	int a;
+	a = 123;
+	int k=0;
+	CString s;
+	CString A;
+	s.Format(_T("我是lhl"), k);
+	A.Format(_T("%d"), a);
+	pDC->TextOutW(100, 200, s);
+	pDC->TextOutW(100, 400, A);
+}
+
+
+// CMFC1View 诊断
+
+#ifdef _DEBUG
+void CMFC1View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFC1View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFC1Doc* CMFC1View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFC1Doc)));
+	return (CMFC1Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFC1View 消息处理程序
---
+++ MFC3/MFC3/MFC3.h
@@ -0,0 +1,33 @@
+﻿
+// MFC3.h: MFC3 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CMFC3App:
+// 有关此类的实现，请参阅 MFC3.cpp
+//
+
+class CMFC3App : public CWinApp
+{
+public:
+	CMFC3App() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFC3App theApp;
