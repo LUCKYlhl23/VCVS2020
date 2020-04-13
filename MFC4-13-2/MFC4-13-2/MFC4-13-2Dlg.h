45b164286d4e06f8ec5cb8b63281785040944e78
LUCKYlhl123 <1291968750@qq.com>
2020-04-13 16:12:43 +0800

    2020—4—13实验2提交

---
+++ MFC4-13-2/MFC4-13-2/MFC4-13-2Dlg.h
@@ -0,0 +1,43 @@
+﻿
+// MFC4-13-2Dlg.h: 头文件
+//
+
+#pragma once
+
+
+// CMFC4132Dlg 对话框
+class CMFC4132Dlg : public CDialogEx
+{
+// 构造
+public:
+	CMFC4132Dlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFC4132_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnEnChangeEdit1();
+	afx_msg void OnEnChangeEdit2();
+	afx_msg void OnEnChangeEdit3();
+	double add1;
+	double add2;
+	double addsum;
+};
