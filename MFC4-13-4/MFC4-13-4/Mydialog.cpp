a21d7c057b50464b78ff8294b7836b5b6832c4cc
LUCKYlhl123 <1291968750@qq.com>
2020-04-14 23:12:00 +0800

    2020—4—14提交4月13日实验4

---
+++ MFC4-13-4/MFC4-13-4/Mydialog.cpp
@@ -0,0 +1,52 @@
+﻿// Mydialog.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "MFC4-13-4.h"
+#include "Mydialog.h"
+#include "afxdialogex.h"
+
+
+// Mydialog 对话框
+
+IMPLEMENT_DYNAMIC(Mydialog, CDialogEx)
+
+Mydialog::Mydialog(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+	, s1(_T(""))
+	, s2(_T(""))
+	, t(0)
+{
+
+}
+
+Mydialog::~Mydialog()
+{
+}
+
+void Mydialog::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1, s1);
+	DDX_Text(pDX, IDC_EDIT3, s2);
+	DDX_Text(pDX, IDC_EDIT2, t);
+}
+
+
+BEGIN_MESSAGE_MAP(Mydialog, CDialogEx)
+	ON_BN_CLICKED(IDC_BUTTON1, &Mydialog::OnBnClickedButton1)
+END_MESSAGE_MAP()
+
+
+// Mydialog 消息处理程序
+
+
+void Mydialog::OnBnClickedButton1()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	UpdateData(true);
+	CString B;
+	B.Format(_T("%d"), t);
+	s2.Format(_T("%s%s"), s1, B);
+	UpdateData(false);
+}
---
+++ MFC4-13-4/MFC4-13-4/Mydialog.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+
+// Mydialog 对话框
+
+class Mydialog : public CDialogEx
+{
+	DECLARE_DYNAMIC(Mydialog)
+
+public:
+	Mydialog(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~Mydialog();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG1 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	CString s1;
+	CString s2;
+	int t;
+	afx_msg void OnBnClickedButton1();
+};
