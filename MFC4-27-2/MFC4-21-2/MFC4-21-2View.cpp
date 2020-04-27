b848f8b2a66df714842d0a260f32c611296473c3
LUCKYlhl123 <1291968750@qq.com>
2020-04-27 16:12:12 +0800

    2020—4—27提交4月27日课堂练习2

---
+++ MFC4-27-2/MFC4-21-2/MFC4-21-2View.cpp
@@ -0,0 +1,127 @@
+﻿
+// MFC4-21-2View.cpp: CMFC4212View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFC4-21-2.h"
+#endif
+
+#include "MFC4-21-2Doc.h"
+#include "MFC4-21-2View.h"
+#include"MyDlg.h"
+#include"string"
+#include"iostream"
+#include"fstream"
+using namespace std;
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFC4212View
+
+IMPLEMENT_DYNCREATE(CMFC4212View, CView)
+
+BEGIN_MESSAGE_MAP(CMFC4212View, CView)
+	
+	ON_COMMAND(ID_FILE_OPEN, &CMFC4212View::OnFileOpen)
+	ON_COMMAND(ID_POPOUT, &CMFC4212View::OnPopout)
+END_MESSAGE_MAP()
+
+// CMFC4212View 构造/析构
+
+CMFC4212View::CMFC4212View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMFC4212View::~CMFC4212View()
+{
+}
+
+BOOL CMFC4212View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFC4212View 绘图
+
+void CMFC4212View::OnDraw(CDC* pDC)
+{
+	CMFC4212Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CMFC4212View 诊断
+
+#ifdef _DEBUG
+void CMFC4212View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFC4212View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFC4212Doc* CMFC4212View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFC4212Doc)));
+	return (CMFC4212Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFC4212View 消息处理程序
+
+
+
+void CMFC4212View::OnFileOpen()
+{
+	// TODO: 在此添加命令处理程序代码
+	CFileDialog cfd(true);
+	int r = cfd.DoModal();
+	CMFC4212Doc* pDoc = GetDocument();
+	if (r == IDOK)
+	{
+		pDoc->filename = cfd.GetPathName();
+		string s;
+		ifstream ifs(pDoc->filename);
+
+		CClientDC dc(this);
+		int x = 20, y = 20;
+		while (ifs >> s)
+		{
+			pDoc->filename = CString(s.c_str());
+			dc.TextOutW(x, y, CString(s.c_str()));
+			y += 30;
+		}
+
+	}
+}
+
+
+void CMFC4212View::OnPopout()
+{
+	// TODO: 在此添加命令处理程序代码
+	MyDlg dlg;
+	int r = dlg.DoModal();
+	if (r == IDOK)
+	{
+
+	}
+}
---
+++ MFC4-27-2/MFC4-21-2/MyDlg.cpp
@@ -0,0 +1,85 @@
+﻿// MyDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "MFC4-21-2.h"
+#include "MyDlg.h"
+#include "afxdialogex.h"
+#include"MFC4-21-2Doc.h"
+#include"MFC4-21-2View.h"
+
+// MyDlg 对话框
+
+IMPLEMENT_DYNAMIC(MyDlg, CDialogEx)
+
+MyDlg::MyDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+MyDlg::~MyDlg()
+{
+}
+
+void MyDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(MyDlg, CDialogEx)
+	ON_BN_CLICKED(IDC_BUTTON1, &MyDlg::OnBnClickedButton1)
+END_MESSAGE_MAP()
+
+
+// MyDlg 消息处理程序
+
+
+void MyDlg::OnBnClickedButton1()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	CMFC4212Doc* pDoc = ((CMFC4212View *)((CFrameWnd *)AfxGetMainWnd())->GetActiveView())->GetDocument();
+	CImage img;
+	img.Load(pDoc->filename);//图片的路径
+	int	sx, sy, w, h;
+	foo(img, sx, sy, w, h);
+	CDC *pDC = GetDlgItem(IDC_STATIC)->GetDC();
+	pDC->SetStretchBltMode(HALFTONE);//KilEH8 1
+	img.Draw(pDC->m_hDC, sx, sy, w, h);
+	ReleaseDC(pDC);
+
+}
+void MyDlg::foo(CImage &img, int &sx, int &sy, int &w, int &h)
+{
+	CRect rect;
+	GetDlgItem(IDC_STATIC)->GetClientRect(&rect);
+	float rect_ratio = 1.0*rect.Width() / rect.Height();
+	float img_ratio = 1.0*img.GetWidth() / img.GetHeight();
+	if (rect_ratio > img_ratio)
+	{
+		h = rect.Height();
+		w = img_ratio * h;
+		sx = (rect.Width() - w) / 2;
+		sy = 0;
+	}
+	else
+	{
+		w = rect.Width();
+		h = w / img_ratio;
+		sx = 0;
+		sy = (rect.Height() - h) / 2;
+	}
+
+}
+
+
+BOOL MyDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// TODO:  在此添加额外的初始化
+	
+	return TRUE;  // return TRUE unless you set the focus to a control
+				  // 异常: OCX 属性页应返回 FALSE
+}
