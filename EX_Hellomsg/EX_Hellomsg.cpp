95aa09f551e118c6cddaf3da845dddf009afc800
LUCKYlhl123 <1291968750@qq.com>
2020-05-11 16:54:07 +0800

    第一周第一个实验

---
+++ EX_Hellomsg/EX_Hellomsg.cpp
@@ -0,0 +1,7 @@
+#include<Windows.h>
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+		MessageBox(NULL, "你好，我的visual C++世界", "问候", 0);
+		return 0;
+}
+	
