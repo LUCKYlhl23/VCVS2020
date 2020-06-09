2eabcf78d6b70bab8cf40930bbe3aa88d30884d7
LUCKYlhl123 <1291968750@qq.com>
2020-06-09 15:37:45 +0800

    2020年6月9日上传综合实验之二第二小题

---
+++ MFweek13-2/MFweek13-2/MFweek13-2.h
@@ -0,0 +1,32 @@
+
+// MFweek13-2.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMFweek132App: 
+// 有关此类的实现，请参阅 MFweek13-2.cpp
+//
+
+class CMFweek132App : public CWinApp
+{
+public:
+	CMFweek132App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFweek132App theApp;
